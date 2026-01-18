771fd8517524f4e79b279ce1588a6c901f0931f7
MxPerrot <44461157+MxPerrot@users.noreply.github.com>
2026-01-18 23:37:49 +0100

    debug

--- alias.lua
+++ alias.lua
@@ -1,4 +1,6 @@
--- create a `pm` alias that delegates to `/vx/pm/init.lua` when available
+-- create an `adduser` alias that delegates to `/vx/os/adduser.lua` when available
 if fs.exists("/vx/os/adduser.lua") then
   pcall(function() shell.setAlias("adduser", "/vx/os/adduser.lua") end)
+else
+  print("No adduser script found at /vx/os/adduser.lua")
 end
